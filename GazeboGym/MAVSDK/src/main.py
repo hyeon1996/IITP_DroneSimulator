720a733b3f818b45572a587fa61ecc9351c272ec
JM-KIM-94 <jmkim22@unist.ac.kr>
2024-04-02 10:14:55 +0900

    implemented step() reset()

--- GazeboGym/MAVSDK/src/main.py
+++ GazeboGym/MAVSDK/src/main.py
@@ -1,28 +1,67 @@
+#!/usr/bin/env python3
+
+
+# https://github.com/m2-farzan/ros2-asyncio
+
 import asyncio
-from Env.MultiAgentEnv import MultiAgentEnv
-from mavsdk import System
+from MultiAgentEnv import MultiAgentEnv
 
-async def f():
-    agent = System()
-    await agent.connect(system_address=f"udp://:14541")
-    async for odometry in agent.telemetry.odometry():
-        print(f"z_m {odometry.position_body.z_m}")
-        # print([odometry.position_body.x_m, odometry.position_body.y_m, odometry.position_body.z_m,
-        #                 odometry.velocity_body.x_m_s, odometry.velocity_body.y_m_s, odometry.velocity_body.z_m_s,
-        #                 odometry.angular_velocity_body.roll_rad_s, odometry.angular_velocity_body.pitch_rad_s,
-        #                 odometry.angular_velocity_body.yaw_rad_s])
+import numpy as np
+import random
+import argparse
+import time
 
-async def main():
-    env = MultiAgentEnv(2,2)
+
+async def main(args):
+
+    env = MultiAgentEnv(args)
+
     await env.setup()
-    await asyncio.sleep(1)
-    await asyncio.sleep(1)
-    await asyncio.sleep(1)
-    await asyncio.sleep(1)
-    await asyncio.sleep(1)
-    await asyncio.sleep(1)
-    await asyncio.sleep(1)
-    await asyncio.sleep(1)
+
+    step = 0
+    while True:
+        t1 = time.time()
+
+        action = np.random.randint(env.n_actions, size=env.n_agents)  # i.e. array([8, 2, 0])
+
+        next_obs, reward, done = await env.step(action)
+        # print(obs)
+
+        print("next_obs", next_obs)
+        print("reward", reward)
+        print("done", done)
+
+        step = step + 1
+
+        await asyncio.sleep(0.01)  # 100 Hz ?
+
+        t2 = time.time()
+        print("loop_time = ", t2 - t1)
+
+
+        if (step % env.episode_limit == 0) or done:
+            print("reset")
+            print("step", step)
+            obs = await env.reset()
+
+
+
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--n_agents', default=3, type=int)
+    parser.add_argument('--episode_limit', default=1000, type=int)
+    parser.add_argument('--px4_git_dir', default="/home/mo/drone/PX4-Autopilot", type=str)
+    parser.add_argument('--mavserver_dir', default="/home/mo/drone/GazeboPX4RL", type=str)
+    parser.add_argument('--goal_margin', default=0.6, type=float)
+    parser.add_argument('--altitude_margin', default=0.3, type=float)
+    parser.add_argument('--rad_per_s_margin', default=1.0, type=float)
+    parser.add_argument('--target_altitude', default=4.0, type=float)
+
+    args, _ = parser.parse_known_args()
+    print("args:", args)
+
+
+    asyncio.run(main(args))
